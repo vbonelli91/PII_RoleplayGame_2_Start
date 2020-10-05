8854d56923ee1903ec7f8734b405451ee43a49ea
vbonelli91 <69222838+vbonelli91@users.noreply.github.com>
2020-10-05 18:06:20 -0300

    Initial commit

---
+++ src/Program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RoleplayGame;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpellsBook book = new SpellsBook();
+            book.Spells = new Spell[]{ new Spell() };
+
+            Wizard gandalf = new Wizard("Gandalf");
+            gandalf.Staff = new Staff();
+            gandalf.SpellsBook = book;
+
+            Dwarf gimli = new Dwarf("Gimli");
+            gimli.Axe = new Axe();
+            gimli.Helmet = new Helmet();
+
+            Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
+            Console.WriteLine($"Gandalf attacks Gimli with ⚔️ {gandalf.AttackValue}");
+
+            gimli.ReceiveAttack(gandalf.AttackValue);
+
+            Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
+
+            gimli.Cure();
+
+            Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
+        }
+    }
+}
